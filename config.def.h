d67e1df6df5c087cea0dd899441298d62fd4e5f2
kwerie <hermsen.rico@gmail.com>
2024-05-25 21:47:45 +0200

    Updated the color scheme

--- config.def.h
+++ config.def.h
@@ -7,10 +7,18 @@ static const char *fonts[] = {
 	"monospace:size=10"
 };
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
+/* // Original color scheme
+static const char *colors[SchemeLast][2] = {
+	//     fg         bg
+	[SchemeNorm] = { "#bbbbbb", "#222222" },
+	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeOut] = { "#000000", "#00ffff" },
+};
+*/
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
 	[SchemeNorm] = { "#bbbbbb", "#222222" },
-	[SchemeSel] = { "#eeeeee", "#005577" },
+	[SchemeSel] = { "#00ff00", "#222222" },
 	[SchemeOut] = { "#000000", "#00ffff" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
